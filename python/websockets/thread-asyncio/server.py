2867c9bec0ada7953b04ad135441c35160a03b71
Seva Ivanov <mail@sevaivanov.com>
2016-07-04 10:49:39 -0400

    py websockets: add ex handling to thread-asyncio

--- python/websockets/thread-asyncio/server.py
+++ python/websockets/thread-asyncio/server.py
@@ -1,7 +1,8 @@
-import asyncio
-import websockets
 import time
 import threading
+import asyncio
+import websockets
+from websockets import exceptions as ws_ex
 
 class ChatServer:
     q = asyncio.Queue()
@@ -15,22 +16,25 @@ class ChatServer:
             self.websockets.append(websocket)
             print('server: adding new socket: %s' % str(websocket))
 
-        print('server: sending welcome to %s' % str(websocket))
+        print('server: sending "welcome" to %s' % str(websocket))
         await websocket.send('welcome')
 
     async def notify(self):
         while True:
             message = await self.q.get()
-            print('server: got %s' % message)
+            print('server: got "%s"' % message)
 
             for websocket in self.websockets:
-                print("server: sending %s to %s" % (message, websocket))
-                await websocket.send(message)
+                print('server: sending "%s" to %s' % (message, websocket))
+                try:
+                    await websocket.send(message)
+                except ws_ex.ConnectionClosed:
+                    print('server: connection closed to %s' % websocket)
     
 def callback_simulation(loop, chat):
     while True:
         time.sleep(3)
-        print('callback: adding hi to queue of size %s' % chat.q.qsize())
+        print('callback: adding "hi" to queue of size %s' % chat.q.qsize())
         loop.call_soon_threadsafe(chat.q.put_nowait, 'hi')
 
 loop = asyncio.get_event_loop()
