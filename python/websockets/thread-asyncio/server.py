3b9e9d3c22653fcc1cff76fe3d1151a6e2445f83
Seva Ivanov <mail@sevaivanov.com>
2016-07-04 14:50:54 -0400

    py ws: add keep ws alive to thread-asyncio

--- python/websockets/thread-asyncio/server.py
+++ python/websockets/thread-asyncio/server.py
@@ -5,13 +5,12 @@ import websockets
 from websockets import exceptions as ws_ex
 
 class ChatServer:
-    q = asyncio.Queue()
+    messages = asyncio.Queue()
 
     def __init__(self):
         self.websockets = list()
 
     async def serve(self, websocket, path):
- 
         if (websocket not in self.websockets):
             self.websockets.append(websocket)
             print('server: adding new socket: %s' % str(websocket))
@@ -19,9 +18,17 @@ class ChatServer:
         print('server: sending "welcome" to %s' % str(websocket))
         await websocket.send('welcome')
 
+        while True:
+            # keeps the websocket alive by the current design
+            # see: https://github.com/aaugustin/websockets/issues/122
+            await asyncio.sleep(60)
+            if (websocket not in self.websockets):
+                print('server: closing websocket %s' % websocket)
+                break
+
     async def notify(self):
         while True:
-            message = await self.q.get()
+            message = await self.messages.get()
             print('server: got "%s"' % message)
 
             for websocket in self.websockets:
@@ -29,13 +36,18 @@ class ChatServer:
                 try:
                     await websocket.send(message)
                 except ws_ex.ConnectionClosed:
+                    self.websockets.remove(websocket)
                     print('server: connection closed to %s' % websocket)
+
     
 def callback_simulation(loop, chat):
     while True:
         time.sleep(3)
-        print('callback: adding "hi" to queue of size %s' % chat.q.qsize())
-        loop.call_soon_threadsafe(chat.q.put_nowait, 'hi')
+        message = 'hi from callback'
+        print('callback: adding "%s" to queue of size %s' % (
+            message, chat.messages.qsize(),
+        ))
+        loop.call_soon_threadsafe(chat.messages.put_nowait, message)
 
 loop = asyncio.get_event_loop()
 chat = ChatServer()
