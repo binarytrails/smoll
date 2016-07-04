2c0d574b08051344478f4f37ffd81b2253a99071
Seva Ivanov <mail@sevaivanov.com>
2016-07-03 22:42:49 -0400

    py websockets: add thread-asyncio draft

---
+++ python/websockets/thread-asyncio/server.py
@@ -0,0 +1,48 @@
+import asyncio
+import websockets
+import time
+import threading
+
+class ChatServer:
+    q = asyncio.Queue()
+
+    def __init__(self):
+        self.websockets = list()
+
+    async def serve(self, websocket, path):
+ 
+        if (websocket not in self.websockets):
+            self.websockets.append(websocket)
+            print('server: adding new socket: %s' % str(websocket))
+
+        print('server: sending welcome to %s' % str(websocket))
+        await websocket.send('welcome')
+
+    async def notify(self):
+        while True:
+            message = await self.q.get()
+            print('server: got %s' % message)
+
+            for websocket in self.websockets:
+                print("server: sending %s to %s" % (message, websocket))
+                await websocket.send(message)
+    
+def callback_simulation(loop, chat):
+    while True:
+        time.sleep(3)
+        print('callback: adding hi to queue of size %s' % chat.q.qsize())
+        loop.call_soon_threadsafe(chat.q.put_nowait, 'hi')
+
+loop = asyncio.get_event_loop()
+chat = ChatServer()
+
+chat_server = websockets.serve(chat.serve, '127.0.0.1', 5678)
+
+loop.create_task(chat_server)
+loop.create_task(chat.notify())
+
+t = threading.Thread(target=callback_simulation, args=(loop, chat,))
+t.start()
+
+loop.run_forever()
+    
