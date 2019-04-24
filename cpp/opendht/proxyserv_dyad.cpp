d1ee2856d0bbd8c5a166a66e7c08127f756be48d
Seva <seva@binarytrails.net>
2019-04-24 11:05:34 -0400

    cpp/opendht: add dyad proxy callbacks to avoid static

--- cpp/opendht/proxyserv_dyad.cpp
+++ cpp/opendht/proxyserv_dyad.cpp
@@ -10,9 +10,15 @@ DhtProxyServer::DhtProxyServer()
 
     dyad_init();
     this->dyadStream = dyad_newStream();
-    dyad_addListener(this->dyadStream, DYAD_EVENT_ERROR,  this->onError,  NULL);
-    dyad_addListener(this->dyadStream, DYAD_EVENT_ACCEPT, this->onAccept, NULL);
-    dyad_addListener(this->dyadStream, DYAD_EVENT_LISTEN, this->onListen, NULL);
+    dyad_addListener(this->dyadStream, DYAD_EVENT_ERROR, [](dyad_Event* event){
+        ((DhtProxyServer*)event->udata)->onError(event);
+    }, this);
+    dyad_addListener(this->dyadStream, DYAD_EVENT_ACCEPT, [](dyad_Event* event){
+        ((DhtProxyServer*)event->udata)->onAccept(event);
+    }, this);
+    dyad_addListener(this->dyadStream, DYAD_EVENT_LISTEN, [](dyad_Event* event){
+        ((DhtProxyServer*)event->udata)->onListen(event);
+    }, this);
     dyad_listen(this->dyadStream, 8080);
 }
 
@@ -23,12 +29,43 @@ DhtProxyServer::~DhtProxyServer()
     delete this->node;
 }
 
-int main(void)
+void DhtProxyServer::onAccept(dyad_Event *event)
 {
-  DhtProxyServer proxy;
-  while (dyad_getStreamCount() > 0)
-  {
-    dyad_update();
-  }
-  return 0;
+    dyad_addListener(event->remote, DYAD_EVENT_LINE, [](dyad_Event* event){
+        ((DhtProxyServer*)event->udata)->onLine(event);
+    }, this);
+}
+
+void DhtProxyServer::onListen(dyad_Event *event)
+{
+    printf("http://localhost:%d\n", dyad_getPort(event->stream));
+}
+
+void DhtProxyServer::onLine(dyad_Event *event)
+{
+    char path[128];
+
+    if (sscanf(event->data, "GET %127s", path) == 1)
+    {
+        printf("%s %s\n", dyad_getAddress(event->stream), path);
+        dyad_writef(event->stream, "HTTP/1.1 200 OK\r\n\r\n");
+        dyad_writef(event->stream, "%s", "dht::get()");
+    }
+
+    dyad_end(event->stream);
+}
+
+void DhtProxyServer::onError(dyad_Event *event)
+{
+    printf("server error: %s\n", event->msg);
+}
+
+int main()
+{
+    DhtProxyServer proxy;
+    while (dyad_getStreamCount() > 0)
+    {
+        dyad_update();
+    }
+    return 0;
 }
--- cpp/opendht/proxyserv_dyad.h
+++ cpp/opendht/proxyserv_dyad.h
@@ -18,30 +18,8 @@ class DhtProxyServer
         dht::DhtRunner *node;
         dyad_Stream *dyadStream;
 
-        static void onLine(dyad_Event *event)
-        {
-            char path[128];
-
-            if (sscanf(event->data, "GET %127s", path) == 1)
-            {
-                printf("%s %s\n", dyad_getAddress(event->stream), path);
-                dyad_writef(event->stream, "HTTP/1.1 200 OK\r\n\r\n");
-                dyad_writef(event->stream, "%s", "dht::get()");
-            }
-
-            dyad_end(event->stream);
-        }
-        static void onAccept(dyad_Event *event)
-        {
-          dyad_addListener(event->remote, DYAD_EVENT_LINE, onLine, NULL);
-        }
-        static void onListen(dyad_Event *event)
-        {
-          printf("http://localhost:%d\n", dyad_getPort(event->stream));
-        }
-        static void onError(dyad_Event *event)
-        {
-          printf("server error: %s\n", event->msg);
-        }
-
+        void onAccept(dyad_Event *event);
+        void onListen(dyad_Event *event);
+        void onLine(dyad_Event *event);
+        void onError(dyad_Event *event);
 };
