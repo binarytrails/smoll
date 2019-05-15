be513b61f298d5f271c5f88bc84127200733ba2e
Seva <seva@binarytrails.net>
2019-05-15 12:28:47 -0400

    cpp/opendht: add resps dispatch queue for get()

--- cpp/opendht/dhtproxyserver/proxy_restinio.h
+++ cpp/opendht/dhtproxyserver/proxy_restinio.h
@@ -12,6 +12,7 @@ using RestRouterTraits = restinio::traits_t<
     restinio::single_threaded_ostream_logger_t,
     RestRouter>;
 using request_status = restinio::request_handling_status_t;
+using response_t = restinio::chunked_output_t;
 
 class DhtProxyServer
 {
@@ -19,9 +20,6 @@ class DhtProxyServer
         DhtProxyServer(std::shared_ptr<dht::DhtRunner> dhtNode,
                        in_port_t port = 8000);
         ~DhtProxyServer();
-
-        template <typename HttpResponse>
-        HttpResponse initHttpResponse(HttpResponse response);
 
         request_status options(restinio::request_handle_t request,
                                restinio::router::route_params_t params);
@@ -33,16 +31,28 @@ class DhtProxyServer
                            restinio::router::route_params_t params);
 
     private:
-        const std::string RESP_MSG_MISSING_PARAMS = "{\"err\":\"Missing parameters\"}";
-        const std::string RESP_MSG_PUT_FAILED = "{\"err\":\"Put failed\"}";
+        template <typename HttpResponse>
+        HttpResponse initHttpResponse(HttpResponse response);
+        typedef std::function<void(void)> response_func;
+
+        std::unique_ptr<RestRouter> createRestRouter();
+        void scheduleRespDispatch(response_func&& response); // dispatch &&move
+        void dispatchResponses();
 
         std::shared_ptr<dht::DhtRunner> dhtNode;
         Json::StreamWriterBuilder jsonBuilder;
         std::thread serverThread {};
+        std::thread serverRespThread {};
+
+        bool stopServer_ = false;
+        std::condition_variable serverRespCv_;
+        std::queue<response_func> serverRespQueue_;
+        std::mutex serverRespLock_;
 
         mutable std::mutex statsMutex;
         mutable dht::NodeInfo dhtNodeInfo {};
         mutable std::atomic<size_t> requestCount {0};
 
-        std::unique_ptr<RestRouter> createRestRouter();
+        const std::string RESP_MSG_MISSING_PARAMS = "{\"err\":\"Missing parameters\"}";
+        const std::string RESP_MSG_PUT_FAILED = "{\"err\":\"Put failed\"}";
 };
