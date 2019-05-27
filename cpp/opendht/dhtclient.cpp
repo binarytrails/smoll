0ffcc12bf0ed446e1acd6de99f0aa9b47b3b6268
Seva <seva@binarytrails.net>
2019-05-27 16:07:39 -0400

    cpp/opendht: add put to proxy client

--- cpp/opendht/dhtclient.cpp
+++ cpp/opendht/dhtclient.cpp
@@ -12,21 +12,28 @@ int main(int argc, char * argv[])
 
     dht::DhtProxyClient client {[](){}, "127.0.0.1:8080", "client_id"};
     std::condition_variable doneCv;
-    bool done = false;
-    dht::Value::Filter filter;
-
-    client.get(hash, [](const dht::Sp<dht::Value>& value){
-        printf("getcb=%s\n", value->toString().c_str());
-        return true;
-    },[&](bool ok){
-        printf("donecb=%i\n", ok);
-        done = true;
-        doneCv.notify_all();
-    });//, std::move(filter));
-
     std::mutex doneMutex;
     std::unique_lock<std::mutex> lock(doneMutex);
-    doneCv.wait_for(lock, std::chrono::seconds(10), [&done]{return done;});
 
+    bool donePut = false;
+    dht::Value value {"slenderman"};
+    client.put(hash, std::move(value), [&](bool ok){
+        printf("put::donecb=%i\n", ok);
+        donePut = true;
+        doneCv.notify_one();
+    });
+    doneCv.wait_for(lock, std::chrono::seconds(10), [&donePut]{return donePut;});
+
+    bool doneGet = false;
+    dht::Value::Filter filter;
+    client.get(hash, [](const dht::Sp<dht::Value>& value){
+        printf("get::cb=%s\n", value->toString().c_str());
+        return true;
+    },[&](bool ok){
+        printf("get::donecb=%i\n", ok);
+        doneGet = true;
+        doneCv.notify_one();
+    }, std::move(filter));
+    doneCv.wait_for(lock, std::chrono::seconds(10), [&doneGet]{return doneGet;});
     return 0;
 }
