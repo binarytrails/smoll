6103eeade4d924dde1a04ef38541104c5d89ba62
Seva <seva@binarytrails.net>
2019-05-28 11:03:38 -0400

    cpp/opendht: add listen to proxy client runner

--- cpp/opendht/dhtclient.cpp
+++ cpp/opendht/dhtclient.cpp
@@ -9,31 +9,44 @@
 int main(int argc, char * argv[])
 {
     auto hash = dht::InfoHash::get(argv[1]);
-
     dht::DhtProxyClient client {[](){}, "127.0.0.1:8080", "client_id"};
     std::condition_variable doneCv;
     std::mutex doneMutex;
     std::unique_lock<std::mutex> lock(doneMutex);
 
-    bool donePut = false;
+    bool doneListen = false;
+    client.listen(hash, [&](const std::vector<dht::Sp<dht::Value>>& values,
+                            bool expired){
+        printf("[listen::cb] values = ");
+        for (const auto &value: values)
+            std::cout << value->toString() << " ";
+        std::cout << std::endl;
+        doneListen = true;
+        doneCv.notify_one();
+        return true;
+    });
+
     dht::Value value {"slenderman"};
+    bool donePut = false;
     client.put(hash, std::move(value), [&](bool ok){
-        printf("put::donecb=%i\n", ok);
+        printf("[put::donecb] ok=%i\n", ok);
         donePut = true;
         doneCv.notify_one();
     });
-    doneCv.wait_for(lock, std::chrono::seconds(10), [&donePut]{return donePut;});
+    doneCv.wait_for(lock, std::chrono::seconds(10), [&]{return donePut;});
 
     bool doneGet = false;
     dht::Value::Filter filter;
     client.get(hash, [](const dht::Sp<dht::Value>& value){
-        printf("get::cb=%s\n", value->toString().c_str());
+        printf("[get::cb] value=%s\n", value->toString().c_str());
         return true;
     },[&](bool ok){
-        printf("get::donecb=%i\n", ok);
+        printf("[get::donecb] ok=%i\n", ok);
         doneGet = true;
         doneCv.notify_one();
     }, std::move(filter));
-    doneCv.wait_for(lock, std::chrono::seconds(10), [&doneGet]{return doneGet;});
+    doneCv.wait_for(lock, std::chrono::seconds(10), [&]{return doneGet;});
+
+    doneCv.wait_for(lock, std::chrono::seconds(20), [&]{return doneListen;});
     return 0;
 }
