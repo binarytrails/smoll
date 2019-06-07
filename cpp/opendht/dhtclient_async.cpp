30dcd55775bc5ab74d8cd3e01667d09c381d8aab
Seva <seva@binarytrails.net>
2019-06-07 15:17:18 -0400

    cpp/opendht: update async proxy client

--- cpp/opendht/dhtclient_async.cpp
+++ cpp/opendht/dhtclient_async.cpp
@@ -13,13 +13,44 @@ int main(int argc, char * argv[])
     auto &io_context = client.httpIOContext();
 
     dht::Value::Filter filter;
+
     client.get(hash, [](const dht::Sp<dht::Value>& value){
-        printf("[get::cb] value=%s\n", value->toString().c_str());
+        printf("[get1::cb] value=%s\n", value->toString().c_str());
         return true;
-    },[&](bool ok){
-        printf("[get::donecb] ok=%i\n", ok);
+    },[](bool ok){
+        printf("[get1::donecb] ok=%i\n", ok);
     }, std::move(filter));
 
-    io_context.run();
+    dht::Value value {"slenderman"};
+    client.put(hash, std::move(value), [&](bool ok){
+        printf("[put1::donecb] ok=%i\n", ok);
+    });
+
+    client.listen(hash, [&](const std::vector<dht::Sp<dht::Value>>& values,
+                            bool expired){
+        printf("[listen1::cb] values = ");
+        for (const auto &value: values)
+            std::cout << value->toString() << " ";
+        std::cout << std::endl;
+        return true;
+    }, std::move(filter));
+
+    client.listen(hash, [&](const std::vector<dht::Sp<dht::Value>>& values,
+                            bool expired){
+        printf("[listen2::cb] values = ");
+        for (const auto &value: values)
+            std::cout << value->toString() << " ";
+        std::cout << std::endl;
+        return true;
+    }, std::move(filter));
+
+    dht::Value value2 {"spidergurl"};
+    client.put(hash, std::move(value2), [&](bool ok){
+        printf("[put2::donecb] ok=%i\n", ok);
+    });
+
+    while (true)
+        io_context.run();
+
     return 0;
 }
