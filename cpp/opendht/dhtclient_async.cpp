e878b622f12d71c13ddb6fd1a4a83f1a5b8ad56e
Seva <seva@binarytrails.net>
2019-06-13 15:09:46 -0400

    cpp/opendht: update async client to new api

--- cpp/opendht/dhtclient_async.cpp
+++ cpp/opendht/dhtclient_async.cpp
@@ -11,46 +11,43 @@ int main(int argc, char * argv[])
     auto hash = dht::InfoHash::get(argv[1]);
 
     std::shared_ptr<dht::Logger> logger = dht::log::getStdLogger();
-    dht::DhtProxyClient client([](){}, "127.0.0.1:8080", "client_id", logger);
-    auto &io_context = client.httpIOContext();
 
-    client.get(hash, [&](const dht::Sp<dht::Value>& value){
+    dht::DhtProxyClient client1([](){}, "127.0.0.1:8080", "client01", logger);
+    dht::DhtProxyClient client2([](){}, "127.0.0.1:8080", "client02", logger);
+
+    // send two posts
+    dht::Value value {"slenderman"};
+    client1.put(hash, std::move(value), [&](bool ok){
+        logger->d("[put1::donecb] ok=%i", ok);
+    });
+
+    dht::Value value2 {"spidergurl"};
+    client1.put(hash, std::move(value2), [&](bool ok){
+        logger->d("[put2::donecb] ok=%i", ok);
+    });
+
+    // schedule one get
+    client1.get(hash, [&](const dht::Sp<dht::Value>& value){
         logger->d("[get1::cb] value=%s", value->toString().c_str());
         return true;
     },[&](bool ok){
         logger->d("[get1::donecb] ok=%i", ok);
     });
 
-    dht::Value value {"slenderman"};
-    client.put(hash, std::move(value), [&](bool ok){
-        logger->d("[put1::donecb] ok=%i", ok);
-    });
-
-    client.listen(hash, [&](const std::vector<dht::Sp<dht::Value>>& values,
+    // start a listener NOTE: blocking operation
+    client2.listen(hash, [&](const std::vector<dht::Sp<dht::Value>>& values,
                             bool expired){
-        std::stringstream ss; ss << "[listen1::cb] values = ";
+        std::stringstream ss; ss << "[listen::cb] values = ";
         for (const auto &value: values)
             ss << value->toString() << " ";
         logger->d(ss.str().c_str());
         return true;
     });
 
-    client.listen(hash, [&](const std::vector<dht::Sp<dht::Value>>& values,
-                            bool expired){
-        std::stringstream ss; ss << "[listen1::cb] values = ";
-        for (const auto &value: values)
-            ss << value->toString() << " ";
-        logger->d(ss.str().c_str());
-        return true;
+    auto t = std::thread([&client2](){
+        client2.io_context().run();
     });
-
-    dht::Value value2 {"spidergurl"};
-    client.put(hash, std::move(value2), [&](bool ok){
-        logger->d("[put2::donecb] ok=%i", ok);
-    });
-
-    while (true)
-        io_context.run();
+    client1.io_context().run();
 
     return 0;
 }
