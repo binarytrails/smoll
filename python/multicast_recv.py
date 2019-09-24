26b62cb50c613675a7a9ff6f4689328f327594a3
Seva <seva@binarytrails.net>
2019-09-24 14:27:24 -0400

    py/mutlicast_recv: fix port casting

--- python/multicast_recv.py
+++ python/multicast_recv.py
@@ -15,7 +15,7 @@ if __name__ == "__main__":
 
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
     sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    sock.bind((source, port))
+    sock.bind((source, int(port)))
     mreq = struct.pack("4sl", socket.inet_aton(group), socket.INADDR_ANY)
     sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
 
