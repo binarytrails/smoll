017c7f382980ac1f68bb956bba3dfffccfc05c3a
Seva <seva@binarytrails.net>
2019-10-14 22:28:33 -0400

    cpp/gnutls: finish ocsp skeleton

--- cpp/gnutls/ocsp.cpp
+++ cpp/gnutls/ocsp.cpp
@@ -7,20 +7,28 @@
 #include <arpa/inet.h>
 #include <sys/socket.h>
 #include <gnutls/gnutls.h>
+#include <gnutls/x509.h>
 #include <assert.h>
 
+#define CHECK(x) assert((x)>=0)
 #define LOOP_CHECK(rval, cmd) \
     do { \
         rval = cmd; \
     } while(rval == GNUTLS_E_AGAIN || rval == GNUTLS_E_INTERRUPTED); \
     assert(rval >= 0)
 
-int main()
+int main(int argc, char* argv[])
 {
-    int port = 8080;
-    std::string host = "127.0.0.1";
-    std::string server_ca = "ca.crt";
-
+    if (argc < 5){
+        printf("./binary <host> <port> <ca.crt> <ca_name>\n");
+        return 1;
+    }
+    int port = atoi(argv[2]);
+    std::string host = argv[1];
+    std::string ca_name = argv[4];
+    std::string server_ca = argv[3];
+    printf("host: %s  port: %u  ca: %s ca_name: %s\n", host.c_str(), port,
+                                                       server_ca.c_str(), ca_name.c_str());
     // Create a socket file descriptor
     int sockfd = 0;
     struct sockaddr_in serv_addr;
@@ -35,83 +43,81 @@ int main()
         printf("error: invalid address not supported\n");
         return 1;
     }
-    if (connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0){ 
-        printf("error: connection failed\n"); 
-        return 1; 
-    }
+    // Initialize GnuTLS
+    CHECK(gnutls_global_init());
+
+    int ret;
+    gnutls_datum_t out;
+    gnutls_session_t session;
 
     // Load the trusted CA certificates.
     gnutls_certificate_credentials_t cred = NULL;
-    int ret = gnutls_certificate_allocate_credentials (&cred);
-    if (ret != GNUTLS_E_SUCCESS){
-        printf("error: gnutls_certificate_allocate_credentials: %s\n",gnutls_strerror(ret));
-        return 1;
-    }
-    // gnutls_certificate_set_x509_system_trust needs GNUTLS version 3.0
-    // or newer, so we hard-code the path to the certificate store
-    // instead.
-    ret = gnutls_certificate_set_x509_trust_file(cred, server_ca.c_str(), GNUTLS_X509_FMT_PEM);
-    if (ret == 0){
-        printf("error: no certificates found in: %s\n", server_ca.c_str());
-        return 1;
-    }
-    if (ret < 0){
-        printf("error: gnutls_certificate_set_x509_trust_files(%s): %s\n",
-               server_ca.c_str(), gnutls_strerror(ret));
-        return 1;
-    }
+    CHECK(gnutls_certificate_allocate_credentials (&cred));
+    CHECK(gnutls_certificate_set_x509_system_trust(cred));
+    CHECK(gnutls_certificate_set_x509_trust_file(cred, server_ca.c_str(), GNUTLS_X509_FMT_PEM));
+
     // Create the session object.
-    gnutls_session_t session;
-    ret = gnutls_init(&session, GNUTLS_CLIENT);
-    if (ret != GNUTLS_E_SUCCESS){
-        printf("error: gnutls_init: %s\n", gnutls_strerror(ret));
-        return 1;
-    }
-    // Configure the cipher preferences.
-    const char *errptr = NULL;
-    ret = gnutls_priority_set_direct(session, "NORMAL", &errptr);
-    if (ret != GNUTLS_E_SUCCESS){
-        printf("error: gnutls_priority_set_direct: %s\nerror: at: \"%s\"\n",
-        gnutls_strerror(ret), errptr);
-        return 1;
-    }
+    CHECK(gnutls_init(&session, GNUTLS_CLIENT));
+    CHECK(gnutls_server_name_set(session, GNUTLS_NAME_DNS, host.c_str(), host.size()));
+    CHECK(gnutls_set_default_priority(session));
+
     // Install the trusted certificates.
-    ret = gnutls_credentials_set(session, GNUTLS_CRD_CERTIFICATE, cred);
-    if (ret != GNUTLS_E_SUCCESS){
-        printf("error: gnutls_credentials_set: %s\n", gnutls_strerror(ret));
+    CHECK(gnutls_credentials_set(session, GNUTLS_CRD_CERTIFICATE, cred));
+    // NOTE deal breaker
+    gnutls_session_set_verify_cert(session, ca_name.c_str(), 0);
+
+    // Establish the session.
+    if (connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0){
+        printf("error: connection failed\n");
         return 1;
     }
     // Associate the socket with the session object and set the server name.
     gnutls_transport_set_ptr(session, (gnutls_transport_ptr_t)(uintptr_t)sockfd);
-    ret = gnutls_server_name_set(session, GNUTLS_NAME_DNS, host.c_str(), host.size());
-    if (ret != GNUTLS_E_SUCCESS){
-        printf("error: gnutls_server_name_set: %s\n",
-        gnutls_strerror(ret));
-        return 1;
+    //gnutls_transport_set_int(session, sockfd);
+    gnutls_handshake_set_timeout(session, GNUTLS_DEFAULT_HANDSHAKE_TIMEOUT);
+
+    // Perform the handshake
+    do {
+        ret = gnutls_handshake(session);
     }
-    // Establish the session.
-    ret = gnutls_handshake(session);
-    if (ret != GNUTLS_E_SUCCESS){
-        printf("error: gnutls_handshake: %s\n", gnutls_strerror(ret));
-        return 1;
+    while (ret < 0 && gnutls_error_is_fatal(ret) == 0);
+    if (ret < 0) {
+        if (ret == GNUTLS_E_CERTIFICATE_VERIFICATION_ERROR){
+            /* check certificate verification status */
+            auto type = gnutls_certificate_type_get(session);
+            int status = gnutls_session_get_verify_cert_status(session);
+            CHECK(gnutls_certificate_verification_status_print(status, type, &out, 0));
+            printf("cert verify output: %s\n", out.data);
+            gnutls_free(out.data);
+        }
+        printf("error: handshake failed: %s\n", gnutls_strerror(ret));
     }
-    // Obtain the server certificate chain.  The server certificate
-    // itself is stored in the first element of the array.
-    unsigned certslen = 0;
-    const gnutls_datum_t *const certs =
-    gnutls_certificate_get_peers(session, &certslen);
-    if (certs == NULL || certslen == 0){
-        printf("error: could not obtain peer certificate\n");
-        return 1;
+    else {
+        // Display session description
+        auto desc = gnutls_session_get_desc(session);
+        printf("gnutls session info: %s\n", desc);
+        gnutls_free(desc);
+        // Safety net
+        printf("tls false start: %s\n", (gnutls_session_get_flags(session) &
+                                         GNUTLS_SFLAGS_FALSE_START) ? "on" : "off");
     }
-    // Validate the certificate chain.
-    unsigned status = (unsigned) -1;
-    ret = gnutls_certificate_verify_peers2(session, &status);
-    if (ret != GNUTLS_E_SUCCESS){
-        printf("error: gnutls_certificate_verify_peers2: %s\n",
-        gnutls_strerror(ret));
-        return 1;
+    gnutls_credentials_type_t c = gnutls_auth_get_type(session);
+    switch (c) {
+    case GNUTLS_CRD_CERTIFICATE:
+        //print_x509_certificate_info(session);
+        printf("todo print x509 info\n");
+        break;
+    default:
+		if ((int) c == -1)
+			printf(("Transport authentication failure\n"));
+		else
+			printf(("Unsupported credential type %d.\n"), (int) c);
+        break;
     }
+
+    /* TODO
+     *  Online Certificate Service Protocol
+     */
 
     // Try sending and receiving some data
     char buf[4096];
