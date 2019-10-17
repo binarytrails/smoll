cf3e5d62c66ef7c2205c314731628dd9b826a455
Seva <seva@binarytrails.net>
2019-10-17 15:48:46 -0400

    cpp/gnutls: make/send valid ocsp request

--- cpp/gnutls/ocsp.cpp
+++ cpp/gnutls/ocsp.cpp
@@ -1,97 +1,73 @@
 // Vsevolod Ivanov
 
-#include <stdlib.h>
-#include <cstring>
 #include <iostream>
-
-#include <unistd.h>
-#include <arpa/inet.h>
-#include <sys/socket.h>
-
-#include <gnutls/gnutls.h>
-#include <gnutls/crypto.h>
-#include <gnutls/x509.h>
-#include <gnutls/ocsp.h>
-
-#include <assert.h>
-#include <vector>
-#include <string>
 #include <fstream>
 #include <streambuf>
-#include <iostream>
-#include <sstream>
+
+#include <gnutls/crypto.h>
+#include <gnutls/ocsp.h>
 
 #include <opendht.h>
 #include <opendht/log.h>
 #include <opendht/http.h>
 
-#define CHECK(x) assert((x)>=0)
-#define LOOP_CHECK(rval, cmd) \
-    do { \
-        rval = cmd; \
-    } while(rval == GNUTLS_E_AGAIN || rval == GNUTLS_E_INTERRUPTED); \
-    assert(rval >= 0)
-
-static gnutls_x509_crt_t load_cert(const std::string& cert_file)
+static gnutls_x509_crt_t load_cert(const char* cert_file, unsigned int crt_max = 1, unsigned flags = 0)
 {
-    printf("Loading certificate: %s\n", cert_file.c_str());
-    gnutls_x509_crt_t crt;
     int ret;
+    size_t size;
+	static gnutls_x509_crt_t *crt;
     gnutls_datum_t data;
-    ret = gnutls_x509_crt_init(&crt);
-    if (ret < 0)
-        exit(1);
-    std::ifstream in;
-    in.open(cert_file);
-    std::stringstream buffer;
-    buffer << in.rdbuf();
-    std::string contents = buffer.str();
-    std::cout << contents << std::endl;
+
+    std::ifstream cert_stream(cert_file, std::ios::binary | std::ios::in);
+    std::string contents((std::istreambuf_iterator<char>(cert_stream)),
+                          std::istreambuf_iterator<char>());
     data.data = (unsigned char*) contents.c_str();
     data.size = contents.size();
-    if (!data.data) {
-        printf("Cannot open file: %s\n", cert_file.c_str());
+    if (!data.data){
+        printf("Cannot open file: %s\n", cert_file);
         exit(1);
     }
-    ret = gnutls_x509_crt_import(crt, &data, GNUTLS_X509_FMT_PEM);
-    if (ret < 0) {
-        printf("Cannot import certificate in %s: %s\n", cert_file.c_str(), gnutls_strerror(ret));
+    ret = gnutls_x509_crt_list_import2(&crt, &crt_max, &data, GNUTLS_X509_FMT_PEM, flags);
+    if (ret < 0){
+        printf("Cannot import certificate in %s: %s\n", cert_file, gnutls_strerror(ret));
         exit(1);
     }
-    return crt;
+    printf("Loaded %d certificates.\n", (int) crt_max);
+    return *crt;
 }
 
-static void ocsp_request(gnutls_datum_t * rdata, gnutls_x509_crt_t cert,
-                         gnutls_x509_crt_t issuer, gnutls_datum_t *nonce)
+void generate_request(gnutls_x509_crt_t cert, gnutls_x509_crt_t issuer,
+		              gnutls_datum_t * rdata, gnutls_datum_t *nonce)
 {
-    gnutls_ocsp_req_t req;
-    int ret = gnutls_ocsp_req_init(&req);
-    if (ret < 0){
-        printf("ocsp_req_init: %s\n", gnutls_strerror(ret));
-        return;
-    }
-    ret = gnutls_ocsp_req_add_cert(req, GNUTLS_DIG_SHA1, issuer, cert);
-    if (ret < 0) {
-        printf("ocsp_req_add_cert: %s\n", gnutls_strerror(ret));
-        return;
-    }
-    ret = gnutls_ocsp_req_add_cert(req, GNUTLS_DIG_SHA1, issuer, cert);
-    if (ret < 0) {
-        printf("ocsp_req_add_cert: %s\n", gnutls_strerror(ret));
-        return;
-    }
-    if (nonce) {
-        ret = gnutls_ocsp_req_set_nonce(req, 0, nonce);
-        if (ret < 0) {
-            printf("ocsp_req_set_nonce: %s\n", gnutls_strerror(ret));
-            return;
-        }
-    }
-    ret = gnutls_ocsp_req_export(req, rdata);
-    if (ret) {
-        printf("ocsp_req_export: %s\n", gnutls_strerror(ret));
-        return;
-    }
+	gnutls_ocsp_req_t req;
+	int ret;
+	ret = gnutls_ocsp_req_init(&req);
+	if (ret < 0){
+		printf( "ocsp_req_init: %s", gnutls_strerror(ret));
+		exit(1);
+	}
+	ret = gnutls_ocsp_req_add_cert(req, GNUTLS_DIG_SHA1, issuer, cert);
+	if (ret < 0){
+		printf( "ocsp_req_add_cert: %s",
+			gnutls_strerror(ret));
+		exit(1);
+	}
+	if (nonce){
+		ret = gnutls_ocsp_req_set_nonce(req, 0, nonce);
+		if (ret < 0){
+			printf( "ocsp_req_set_nonce: %s",
+				gnutls_strerror(ret));
+			exit(1);
+		}
+	}
+	ret = gnutls_ocsp_req_export(req, rdata);
+	if (ret != 0){
+		printf( "ocsp_req_export: %s",
+			gnutls_strerror(ret));
+		exit(1);
+	}
+	gnutls_ocsp_req_deinit(req);
+	return;
 }
 
 static int ocsp_response(gnutls_datum_t * data, gnutls_x509_crt_t cert,
@@ -114,7 +90,7 @@ static int ocsp_response(gnutls_datum_t * data, gnutls_x509_crt_t cert,
     if (ret < 0)
         exit(1);
     if (rnonce.size != nonce->size || memcmp(nonce->data, rnonce.data,
-        nonce->size) != 0) {
+        nonce->size) != 0){
         exit(1);
     }
     ret = gnutls_ocsp_resp_verify_direct(resp, signer, &verify, 0);
@@ -158,6 +134,7 @@ int main(int argc, char* argv[])
     std::string cert_file = argv[1];
     std::string issuer_file = argv[2];
     std::string signer_file = argv[3];
+    std::string request_file = argv[4];
     std::string hostname;
     if (argc > 4)
         hostname = argv[4];
@@ -166,43 +143,49 @@ int main(int argc, char* argv[])
             cert_file.c_str(), issuer_file.c_str(), signer_file.c_str(), hostname.c_str());
 
     // Initialize GnuTLS
-    CHECK(gnutls_global_init());
+    ret = gnutls_global_init();
+    if (ret < 0)
+        printf( "gnutls_global_init: %s\n", gnutls_strerror(ret));
 
     gnutls_x509_crt_t cert, issuer, signer;
-    cert = load_cert(cert_file);
-    issuer = load_cert(issuer_file);
-    signer = load_cert(signer_file);
+    // load_cert(file, max, flags |= GNUTLS_X509_CRT_LIST_SORT
+    cert = load_cert(cert_file.c_str());
+    issuer = load_cert(issuer_file.c_str());
+    //signer = load_cert(signer_file.c_str());
 
-    // Extracts the Authority Information Access (AIA) extension, see RFC 5280 section 4.2.2.1
-    gnutls_datum_t tmp;
-    ret = gnutls_x509_crt_get_authority_info_access(cert, 0, GNUTLS_IA_OCSP_URI, &tmp, NULL);
-    switch (ret){
-        case 0:
-            printf("CA issuers OCSP URI: %s\n", tmp.data);
-            //gnutls_free(tmp.data);
-            break;
-        case GNUTLS_E_UNKNOWN_ALGORITHM:
-        case GNUTLS_E_REQUESTED_DATA_NOT_AVAILABLE:
-            printf("Error: NO URI found in certificate\n");
-            return 1;
-        default:
-            printf("Error: %s\n", gnutls_strerror(ret));
-            break;
+    for (int seq = 0;; seq++)
+    {
+        std::cout << "seq: " << seq << std::endl;
+        // Extracts the Authority Information Access (AIA) extension, see RFC 5280 section 4.2.2.1
+        gnutls_datum_t tmp;
+        ret = gnutls_x509_crt_get_authority_info_access(cert, seq, GNUTLS_IA_OCSP_URI, &tmp, NULL);
+        if (ret == GNUTLS_E_UNKNOWN_ALGORITHM)
+            continue;
+        if (ret == GNUTLS_E_REQUESTED_DATA_NOT_AVAILABLE){
+            printf("No URI was found in the certificate.\n");
+            exit(1);
+        }
+        if (ret < 0){
+            printf( "error: %s\n", gnutls_strerror(ret));
+            exit(1);
+        }
+        printf("CA issuers URI: %.*s\n", tmp.size, tmp.data);
+        gnutls_free(tmp.data);
+        break;
     }
 
     // Generate OCSP request
     unsigned char noncebuf[23];
     gnutls_datum_t nonce = { noncebuf, sizeof(noncebuf) };
     ret = gnutls_rnd(GNUTLS_RND_NONCE, nonce.data, nonce.size);
-    if (ret < 0) {
+    if (ret < 0){
         printf("gnutls_rnd: %s\n", gnutls_strerror(ret));
         return -1;
     }
     gnutls_datum_t ocsp_req;
-    ocsp_request(&ocsp_req, cert, issuer, &nonce);
-
-    std::string ocsp_req_str = reinterpret_cast<char*>(ocsp_req.data);
-    printf("OCSP Request: %s\n", ocsp_req_str.c_str());
+	generate_request(cert, issuer, &ocsp_req, &nonce);
+	gnutls_x509_crt_deinit(cert);
+	gnutls_x509_crt_deinit(issuer);
 
     // Send OCSP request
     using namespace dht;
@@ -215,8 +198,14 @@ int main(int argc, char* argv[])
     request->set_header_field(restinio::http_field_t::user_agent, "OCSP client");
     request->set_header_field(restinio::http_field_t::accept, "*/*");
     request->set_header_field(restinio::http_field_t::content_type, "application/ocsp-request");
-
-    request->set_body(reinterpret_cast<char*>(ocsp_req.data));
+    {
+        std::stringstream body;
+        body.write(ocsp_req.data, ocsp_req.size);
+        std::cout << body.str() << std::endl;
+        gnutls_free(ocsp_req.data);
+        request->set_body(body.str());
+    }
+    request->set_connection_type(restinio::http_connection_header_t::close);
 
     std::weak_ptr<http::Request> wreq = request;
     request->add_on_state_change_callback([logger, &io_context, wreq]
@@ -224,6 +213,8 @@ int main(int argc, char* argv[])
         logger->w("HTTP Request state=%i status_code=%i", state, response.status_code);
         auto request = wreq.lock();
         logger->w("HTTP Request:\n%s", request->to_string().c_str());
+        if (state == http::Request::State::SENDING)
+            request->get_connection()->timeout(std::chrono::seconds(1)); // fixme
         if (state != http::Request::State::DONE)
             return;
         if (response.status_code != 200)
@@ -241,9 +232,6 @@ int main(int argc, char* argv[])
     //int v = ocsp_response(&ud, cert, signer, &nonce);
     //printf("= verified: %i\n", v);
 
-    gnutls_x509_crt_deinit(cert);
-    gnutls_x509_crt_deinit(issuer);
-    gnutls_x509_crt_deinit(signer);
     gnutls_global_deinit();
 
     printf("= success\n");
