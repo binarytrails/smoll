015cb36b1dc3b9947f2bcaf8d7b681a35b02d296
Seva <binarytrails@users.noreply.github.com>
2021-03-20 21:57:21 -0400

    win/powershell: fit all msvc versions & confirm install

--- win/powershell/install_vsbuildtools.ps1
+++ win/powershell/install_vsbuildtools.ps1
@@ -1,5 +1,6 @@
 mkdir /install -Force
 Invoke-WebRequest https://aka.ms/vs/16/release/vs_buildtools.exe  -OutFile /install/vs_buildtools.exe
 /install/vs_buildtools.exe --passive --wait --norestart --nocache --installPath C:\BuildTools --add Microsoft.VisualStudio.Workload.VCTools --add Microsoft.VisualStudio.Component.VC.CMake.Project --add Microsoft.VisualStudio.Component.VC.CoreBuildTools --add Microsoft.VisualStudio.Component.VC.Llvm.Clang --add Microsoft.VisualStudio.Component.VC.Tools.x86.x64 --add Microsoft.VisualStudio.Workload.MSBuildTools --includeRecommended
-while (!(Test-Path "C:/BuildTools/VC/Tools/MSVC/14.27.29110/bin/Hostx64/x64/cl.exe")){ echo "Waiting for vs_buildtools to complete for a valid cl.exe compiler... Sleeping..."; Start-Sleep 60 }
-cmake -G "Visual Studio 16 2019" -DCMAKE_BUILD_TYPE=Release ../
+while (!(Test-Path "C:/BuildTools/VC/Tools/MSVC/*/bin/Hostx64/x64/cl.exe")){ echo "Waiting for vs_buildtools to complete for a valid cl.exe compiler... Sleeping..."; Start-Sleep 60 }
+echo "Windows Build Tools installed!"
+echo 'Example command: cmake -G "Visual Studio 16 2019" -DCMAKE_BUILD_TYPE=Release ../'
