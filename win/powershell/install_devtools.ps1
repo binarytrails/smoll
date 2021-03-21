8361c48b3033f89cdef816e64f03f1bae8796990
Seva <binarytrails@users.noreply.github.com>
2021-03-20 21:42:09 -0400

    win/powershell: add install dev & vsbuild tools

---
+++ win/powershell/install_devtools.ps1
@@ -0,0 +1,4 @@
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+choco install -y git
+$Env:path += ";C:\Program Files\Git\bin"
+choco install -y vim
