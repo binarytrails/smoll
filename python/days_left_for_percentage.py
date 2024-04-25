7e04415253dcca30eb6f81ab04c4f76109a6251b
seva <5713600+binarytrails@users.noreply.github.com>
2024-04-25 17:20:01 -0400

    python: add days_left_for_percentage.py

---
+++ python/days_left_for_percentage.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+import sys
+from datetime import datetime as dt
+
+date_format = "%m/%d/%Y"
+end_date = dt.strptime(sys.argv[1], date_format)
+
+days = (end_date - dt.today()).days
+percentage = (100 - float(sys.argv[2])) / (days)
+
+print('days_left_for_percentage.py <end-date> <% completed>')
+print('i.e.: days_left_for_percentage.py 6/16/2024 50')
+print('\ndays left: {}\ntempo per day: {:.2f}%'.format(days, percentage))
