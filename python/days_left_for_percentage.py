96b62a15946d5cd1a819f49da868cf50ec01629e
seva <5713600+binarytrails@users.noreply.github.com>
2024-04-26 16:15:40 -0400

    days_left: sexyer :octopus:

--- python/days_left_for_percentage.py
+++ python/days_left_for_percentage.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-
+#
+# print('days_left_for_percentage.py <end-date> <% completed>')
+# print('i.e.: days_left_for_percentage.py 6/16/2024 50')
+#
 import sys
 from datetime import datetime as dt
 
@@ -8,7 +11,4 @@ end_date = dt.strptime(sys.argv[1], date_format)
 
 days = (end_date - dt.today()).days
 percentage = (100 - float(sys.argv[2])) / (days)
-
-print('days_left_for_percentage.py <end-date> <% completed>')
-print('i.e.: days_left_for_percentage.py 6/16/2024 50')
-print('\ndays left: {}\ntempo per day: {:.2f}%'.format(days, percentage))
+print('days left: {}\ntempo per day: {:.2f}%'.format(days, percentage))
